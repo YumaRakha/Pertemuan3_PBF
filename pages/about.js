d108806f78da72aff3407780c21b1e0aaa978117
yuma rakha samodra sikayo <sikayoyuma@gmail.com>
2025-03-01 13:59:46 +0700

    praktikum selesai

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>Tentang Kami</h1>
+            <p>Kami adalah perusahaan yang mengkhususkakn diri dalam pembuatan website berkualiatas tinggi.</p>
+        </div>
+    );
+};
+
+export default AboutPage;
